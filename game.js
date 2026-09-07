bc8a70205e23d5db82a4da2d10f55260826a922f
DG-1000 <dave.grom@outlook.com>
2026-09-07 11:18:47 -0700

    Added respawning

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const player = {
 // Keyboard input
 const keys = {};
 
-const platforms = [[0,350,400,400],[200,550,800,600]];
+const platforms = [[0,350,300,400],[200,550,800,600]];
 
 document.addEventListener("keydown", (event) => {
     keys[event.key.toLowerCase()] = true;
@@ -45,6 +45,11 @@ function update() {
     if (inPlatform() && keys["w"]) player.ySpeed = -25;
     if (keys["a"]) player.x -= player.speed;
     if (keys["d"]) player.x += player.speed;
+    if (player.y >= 1000) {
+        player.x = 400;
+        player.y = 300;
+        player.ySpeed = 0;
+    }
 }
 
 // Draw everything
