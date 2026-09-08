5af614642f96092fd47997c959b84512189a4e1a
DG-1000 <dave.grom@outlook.com>
2026-09-07 18:54:30 -0700

    Added random world generation

--- game.js
+++ game.js
@@ -14,7 +14,17 @@ const player = {
 // Keyboard input
 const keys = {};
 
-const platforms = [[-50,350,300,400],[200,550,850,600],[350,150,550,200]];
+
+const mapSize = 64;
+const blocks = Array.from({length: mapSize}, () => Array(mapSize).fill(false));
+for (let i = 0; i < Math.floor(mapSize**2/50); i++) {
+    let a = Math.floor((Math.random())*(mapSize-2));
+    let b = Math.floor(Math.random()**2*mapSize);
+    console.log(a,b)
+    blocks[a][b] = true;
+    blocks[a+1][b] = true;
+    blocks[a+2][b] = true;
+}
 const offset = 400-player.width/2
 
 document.addEventListener("keydown", (event) => {
@@ -29,21 +39,19 @@ const inRange = (num, min, max) => num >= min && num <= max;
 
 function inPlatform() {
     if (player.y+player.height > canvas.height) return true;
-    let x = player.x+player.width/2
-    let y = player.y+player.height
-    for (let i = 0; i < platforms.length; i++) {
-        platform = platforms[i];
-        if (inRange(x, platform[0], platform[2])) {
-            if (inRange(y, platform[1], platform[3])) return(true);
-        }
+    let x = Math.floor((player.x+player.width/2)/50);
+    let y = Math.floor((player.y+player.height-50)/-50)+11;
+    try {
+        if (blocks[x+mapSize/2][y] == true) return true;
     }
+    catch (error) {}
 }
 
 // Update player
 function update() {
     if (inPlatform() && player.ySpeed >= 0) player.ySpeed = 0
     player.y += player.ySpeed;
-    if (player.ySpeed < 20) player.ySpeed += 1;
+    if (player.ySpeed < 25) player.ySpeed += 1;
     if (inPlatform() && keys["w"]) player.ySpeed = -25;
     if (keys["a"]) player.x -= player.speed;
     if (keys["d"]) player.x += player.speed;
@@ -69,13 +77,16 @@ function draw() {
         player.height
     );
     ctx.fillStyle = "black";
-    for (let i = 0; i < platforms.length; i++) {
-        ctx.fillRect(
-            platforms[i][0]+offset-player.x,
-            platforms[i][1]+yOffset-player.y,
-            platforms[i][2]-platforms[i][0],
-            platforms[i][3]-platforms[i][1]
-        );
+    for (let i = 0; i < mapSize; i++) {
+        for (let j = 0; j < mapSize; j++) {
+            if (blocks[i][j] != true) continue;
+            ctx.fillRect(
+                i*50+offset-player.x-mapSize*25,
+                550+j*-50+yOffset-player.y,
+                51,
+                51
+            );
+        }
     }
 }
 
